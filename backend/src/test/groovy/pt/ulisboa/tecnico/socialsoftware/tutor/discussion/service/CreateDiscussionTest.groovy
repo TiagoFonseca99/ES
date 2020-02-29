38910a0e6ddd8374c940250669b823e2b071defc
DMaxter <daniel_matos@outlook.pt>
2020-02-29 23:09:49 +0000

    Test cases reviewed

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/service/CreateDiscussionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/service/CreateDiscussionTest.groovy
@@ -4,25 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import pt.ulisboa.tecnico.socialsoftware.tutor.discussion.DiscussionService
-import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import spock.lang.Specification
 
 @DataJpaTest
 class CreateDiscussionTest extends Specification {
-    def "create discussion not null"(){
+
+    }
+
+    def "create discussion"(){
         expect: false
     }
 
-    def "question not null"(){
-        expect: false
-    }
-
-    def "question exists"(){
-        expect: false
-    }
-
-    def "student enrolled in question's course"(){
+    def "ensure user is student"(){
         expect: false
     }
 
