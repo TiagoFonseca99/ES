59f1edb11e16eaeb2a3a283b079a3bbe756a5b86
jdinis99 <joao.t.dinis@gmail.com>
2020-03-04 03:45:02 -0800

    Testes Definidos

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/SeeOpenedTournamentsTest.groovy
@@ -0,0 +1,49 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import spock.lang.Specification
+
+import java.time.LocalDateTime
+
+@DataJpaTest
+class SeeOpenedTournamentsTest extends Specification {
+
+    def setup() {
+
+    }
+
+    def "criar 2 torneios abertos e ver abertos"() {
+
+    }
+
+    def "criar 2 torneios abertos e 1 fechado e ver abertos"() {
+
+    }
+
+    def "criar 0 torneios e ver abertos"() {
+
+    }
+
+    def "criar 1 torneio fechado e ver abertos"() {
+
+    }
+
+
+
+}
