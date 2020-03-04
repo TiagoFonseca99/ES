2a6588bdf229b2ec4cf358a2e38dd732fa4f1bd4
DMaxter <daniel_matos@outlook.pt>
2020-03-04 02:01:15 +0000

    Discussion domain

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/domain/Discussion.java
@@ -0,0 +1,56 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.discussion.domain;
+
+import javax.persistence.*;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.*;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+
+@Entity
+@Table(
+       name = "discussions"
+)
+
+public class Discussion {
+    @SuppressWarnings("unused")
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique=true, nullable = false)
+    private Integer key;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Question question;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
