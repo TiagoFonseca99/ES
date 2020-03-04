5c2c577458a8033215868c43d6e2389cafa60c8a
DMaxter <daniel_matos@outlook.pt>
2020-03-04 16:00:01 +0000

    Basic implementation Discussion Service

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/DiscussionService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/DiscussionService.java
@@ -1,5 +1,10 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.discussion;
 
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
@@ -7,7 +12,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.discussion.dto.DiscussionDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.discussion.repository.DiscussionRepository;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.QuestionRepository;
+
 @Service
 public class DiscussionService {
-    
+    @Autowired
+    private DiscussionRepository discussionRepository;
+
+    @Autowired
+    private QuestionRepository QuestionRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Retryable(value = { SQLException.class }, backoff = @Backoff(delay = 5000))
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public DiscussionDto createDiscussion(Integer id, DiscussionDto discussionDto) {
+        // TODO implement
+        return discussionDto;
+    }
 }
