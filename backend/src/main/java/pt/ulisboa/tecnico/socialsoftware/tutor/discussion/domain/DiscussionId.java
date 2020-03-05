60ad66871a75f1f5f44395bb3da49de189ffca84
DMaxter <daniel_matos@outlook.pt>
2020-03-05 02:12:38 +0000

    Discussion Primary Key

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/domain/DiscussionId.java
@@ -0,0 +1,34 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.discussion.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+
+@Embeddable
+public class DiscussionId implements Serializable {
+    @NotNull
+    @Column(name="question_id")
+    private Integer questionId;
+
+    @NotNull
+    @Column(name="user_id")
+    private Integer userId;
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+}
