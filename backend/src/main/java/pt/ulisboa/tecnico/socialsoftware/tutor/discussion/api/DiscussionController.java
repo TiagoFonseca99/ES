4ec2d3b03488b5de98ccc28ef3671c2971c15c44
DMaxter <daniel_matos@outlook.pt>
2020-03-24 01:21:18 +0000

    Discussion Web Service Tests Closes #83

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/api/DiscussionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/api/DiscussionController.java
@@ -1,17 +1,23 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.discussion.api;
 
+import java.security.Principal;
+
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.discussion.DiscussionService;
 import pt.ulisboa.tecnico.socialsoftware.tutor.discussion.dto.DiscussionDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
 
 @RestController
 public class DiscussionController {
@@ -21,8 +27,16 @@ public class DiscussionController {
     private DiscussionService discussionService;
 
     @PostMapping(value = "/discussions")
-    @PreAuthorize("hasROLE('ROLE_STUDENT')")
-    public DiscussionDto createDiscussion(@Valid @RequestBody DiscussionDto discussion){
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    public DiscussionDto createDiscussion(Principal principal, @Valid @RequestBody DiscussionDto discussion){
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if (user == null) {
+            throw new TutorException(ErrorMessage.AUTHENTICATION_ERROR);
+        }
+
+        discussion.setUserId(user.getId());
+
         return discussionService.createDiscussion(discussion);
     }
 }
