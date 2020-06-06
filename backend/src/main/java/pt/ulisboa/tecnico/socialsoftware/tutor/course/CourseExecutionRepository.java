25543355977e71bdb617a178fc0cfc108f757b2a
Daniel Serafim <48388827+dserafim1999@users.noreply.github.com>
2020-06-06 16:51:10 +0100

    Update CourseExecutionRepository.java

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/course/CourseExecutionRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/course/CourseExecutionRepository.java
@@ -1,12 +1,8 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import pt.ulisboa.tecnico.socialsoftware.tutor.submission.domain.Submission;
-
-import java.util.List;
 
 @Repository
 @Transactional
